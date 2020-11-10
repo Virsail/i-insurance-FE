91c7437335e5b98e99d9cf96454b565eb348d597
caiseyann <caiseyann4@gmail.com>
2020-11-10 14:49:10 +0300

    updated folder structure

---
+++ src/app/layout/app-custom-layout/app-custom-layout.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppCustomLayoutComponent } from './app-custom-layout.component';
+
+describe('AppCustomLayoutComponent', () => {
+  let component: AppCustomLayoutComponent;
+  let fixture: ComponentFixture<AppCustomLayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppCustomLayoutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppCustomLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/layout/app-custom-layout/app-custom-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-custom-layout',
+  templateUrl: './app-custom-layout.component.html',
+  styleUrls: ['./app-custom-layout.component.css']
+})
+export class AppCustomLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
