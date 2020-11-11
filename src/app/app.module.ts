69786209de43a268084100cb21504278f4cde469
virsail <virsaileric@gmail.com>
2020-11-11 19:09:45 +0300

    update angular cli version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { AppCustomLayoutComponent } from './layout/app-custom-layout/app-custom-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppCustomLayoutComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HelloComponent ,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgImageSliderModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
