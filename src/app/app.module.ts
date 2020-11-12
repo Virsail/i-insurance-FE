1465158dfdf0b70108603ea5c6a85a93178fac04
virsail <virsaileric@gmail.com>
2020-11-12 22:52:28 +0300

    fix the error in component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgImageSliderModule } from 'ng-image-slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AppCustomLayoutComponent } from './layout/app-custom-layout/app-custom-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+
+
 
 @NgModule({
   declarations: [
@@ -15,13 +18,13 @@ import { HelloComponent } from './hello.component';
     AppCustomLayoutComponent,
     HeaderComponent,
     FooterComponent,
-    HelloComponent ,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgImageSliderModule,
+    RouterModule,
     
 
   ],
