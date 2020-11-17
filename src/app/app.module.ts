ba9a273dd9dfb378b62efcbed1e84466e08020ba
caiseyann <caiseyann4@gmail.com>
2020-11-17 09:03:18 +0300

    about us, contact us and other nav links functioning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppCustomLayoutComponent } from './layout/app-custom-layout/app-custom-layout.component';
-import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TestimonyComponent } from './testimony/testimony.component';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +23,20 @@ import { TestimonyComponent } from './testimony/testimony.component';
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
-    TestimonyComponent
+    TestimonyComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProfileComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    AuthModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
