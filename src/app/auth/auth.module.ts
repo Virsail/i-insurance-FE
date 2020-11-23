a94d1c0e9483473556f405ef94c0b73df75685ee
virsail <virsaileric@gmail.com>
2020-11-24 02:24:00 +0300

    fix the login api

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,36 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HomekComponent } from './components/homek/homek.component';
-import { FooterkComponent } from './components/footerk/footerk.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JwtInterceptor } from './components/helpers';
+
 
 @NgModule({
-  declarations: [LoginComponent, ResetPasswordComponent, RegisterComponent, HomekComponent, FooterkComponent],
-
-
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-],
+  declarations: [LoginComponent, ResetPasswordComponent],
   imports: [
-    BrowserModule,
     CommonModule,
-    ReactiveFormsModule,
-    AuthRoutingModule,
-    HttpClientModule
+    AuthRoutingModule
   ],
   exports:[
     LoginComponent, 
-    ResetPasswordComponent,
-    RegisterComponent
+    ResetPasswordComponent
   ]
 })
-
-
 export class AuthModule { }
